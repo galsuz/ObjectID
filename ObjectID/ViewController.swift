809aa451061b964c61917ad7e8e5ec3972d40258
Alsu Giniatullina <g.alsu.z.212@gmail.com>
2021-04-29 15:40:37 +0300

    Add ObjectID

---
+++ ObjectID/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  ObjectID
+//
+//  Created by Алсу Гиниятуллина  on 28.04.2021.
+//
+
+import UIKit
+import AVKit
+import Vision
+import CoreML
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet weak var belowView: UIView!
+    @IBOutlet weak var objectNameLabel: UILabel!
+    @IBOutlet weak var accuracyLabel: UILabel!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    var model = Resnet50().model
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        
+        view.addSubview(belowView)
+        
+        belowView.clipsToBounds = true
+        belowView.layer.cornerRadius = 15.0
+        belowView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        
+        
+        let  dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let model = try? VNCoreMLModel(for: model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
+            guard let firstObservation = results.first else {return}
+            
+            let name: String = firstObservation.identifier
+            let acc: Int = Int(firstObservation.confidence * 100)
+            
+            DispatchQueue.main.async {
+                self.objectNameLabel.text = name
+                self.accuracyLabel.text = "Accuracy: \(acc)%"
+            }
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+}
+
